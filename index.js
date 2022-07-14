72df0cc9555a423b115696c4ae7bfcef4224ced2
mdbappa0001 <abutanif366@gmail.com>
2022-07-14 14:53:51 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -10,87 +11,97 @@ app.use(cors());
 app.use(express.json());
 
 
-
+// jwt function 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" })
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" })
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdcuw.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-try{
-  await client.connect();
-  const serviceCollection = client.db('furniture_mart').collection('services');
-
-app.get('/service', async(req,res)=>{
-  const query = {};
-  const cursor = serviceCollection.find(query);
-  const services = await cursor.toArray();
-  res.send(services);
-})
-
- //get one data by Id------------ 
- app.get('/service/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) }
-  const service = await serviceCollection.findOne(query)
-  res.send(service)
-});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('furniture_mart').collection('services');
 
 
- // get data by email 
- app.get('/my-items', verifyJWT, async (req, res) => {
-  const decodedEmail = req.decoded.email;
-  const email = req.query.email;
-  if (email === decodedEmail) {
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    //get one data by Id------------ 
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const service = await serviceCollection.findOne(query)
+      res.send(service)
+    });
+
+
+    // get data by email 
+    app.get('/my-items', async (req, res) => {
+      const email = req.query.email;
       const query = { email: email }
       const cursor = serviceCollection.find(query)
       const result = await cursor.toArray()
       res.send(result)
-  } else {
-      res.send(403).send({ message: 'Forbidden Access' })
-  }
-})
+    })
 
 
- // update ---------------------
- app.put('/update/:id', async (req, res) => {
-  const id = req.params;
-  const updatedQuantity = req.body;
-  const filter = { _id: ObjectId(id) }
-  const options = { upsert: true }
-  const updatedDoc = {
-      $set: {
+    // update ---------------------
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params;
+      const updatedQuantity = req.body;
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true }
+      const updatedDoc = {
+        $set: {
           quantity: updatedQuantity.newQuantityTotal
+        }
       }
+      const result = await serviceCollection.updateOne(filter, updatedDoc, options)
+      res.send(result)
+    });
+
+
+    // post & update data **********************
+    // post data ----------------------
+    app.post('/service', async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service)
+      res.send(result)
+    })
+
+
+    // delete a data by id ------------------------
+    app.delete('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
   }
-  const result = await serviceCollection.updateOne(filter, updatedDoc, options)
-  res.send(result)
-});
+  finally {
 
-
-   // post & update data **********************
-        // post data ----------------------
-        app.post('/service', async (req, res) => {
-          const service = req.body;
-          const result = await serviceCollection.insertOne(service)
-          res.send(result)
-      })
-
-
- // delete a data by id ------------------------
- app.delete('/service/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) }
-  const result = await serviceCollection.deleteOne(query)
-  res.send(result)
-})
-
-
-}
-finally{
-
-}
+  }
 }
 
 run().catch(console.dir);
