1c6438f313f3c43138f492d1f1480bb660a87999
mdbappa0001 <abutanif366@gmail.com>
2022-07-14 12:21:11 +0600

    deleted api added

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ app.get('/service', async(req,res)=>{
   }
   const result = await serviceCollection.updateOne(filter, updatedDoc, options)
   res.send(result)
+});
+
+
+ // delete a data by id ------------------------
+ app.delete('/service/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) }
+  const result = await serviceCollection.deleteOne(query)
+  res.send(result)
 })
 
 
